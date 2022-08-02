3deb24600aed1b54c09bcd91667e44ff62dd5b7a
NishiyamaSora <nishiyamasora0205@gmail.com>
2022-08-02 19:38:20 +0900

    [delete]ブログ投稿

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,14 +13,29 @@
             {{ $post->title }}
         </h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_delete" method="post" style="display:inline">
+             @csrf
+             @method('DELETE')
+             <input type="submit" style="display:none">
+             <a class='detele'>[<span onclick="return deletePost(this);">delete</span>]</a>
+        </form>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
-                <p>{{ $post->body }}</p>    
+                <p>{{ $post->body }}</p> 
+                <p class='updated_at'>{{ $post->updated_at }}</p>
             </div>
         </div>
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        <script>
+            function deletePost(e) {
+                'use strict';
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById('form_delete').submit();
+                }
+            }
+        </script>
     </body>
 </html>
